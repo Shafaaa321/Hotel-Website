293cc441b3b7dc5546ab77bc8218ec1dc0a81fed
Shafaatu Hamza <shafaatuhamza22@icloud.com>
2024-11-22 19:46:21 +0100

    Added styles, texts, images

--- script.js
+++ script.js
@@ -13,3 +13,27 @@ document.addEventListener("DOMContentLoaded", function() {
         });
     }
 });
+
+const chooseUsArray = [
+    {image: 'items/img.svg', title: "Payment method", description: "We have a lot of them, from <br> cryptocurrencies to barter for potatoes"},
+    {},
+    {}
+]
+
+const renderChooseUs = () => {
+    const chooseUsTag = document.getElementById('')
+    
+    chooseUsArray.forEach((element) => {
+        chooseUsTag.innerHTML += `
+            <div class="choose-us-element">
+                <img src=${element.image} class="pic6">
+                <h3>${element.title}</h3>
+                <p class="para">${element.description}</p>
+            </div> 
+        `
+    })
+}
+
+
+renderChooseUs()
+
